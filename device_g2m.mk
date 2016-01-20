3f9c2ea34dd05fac76fbf9eaa42cc2c8c8194902
vm03 <vm03@yandex.ru>
2016-01-20 16:42:06 +0300

    twrp: add TWRP support
    Change-Id: I0866ad9ffc9a6526c2d3d98b2d1368c9d28d18a9

--- device_g2m.mk
+++ device_g2m.mk
@@ -33,6 +33,23 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/lge/g2m/releasetools/fixup.sh:install/bin/fixup.sh
 
+
+# TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/twrp/vendor/firmware/cmnlib.b00:recovery/root/system/vendor/firmware/cmnlib.b00 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/cmnlib.b01:recovery/root/system/vendor/firmware/cmnlib.b01 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/cmnlib.b02:recovery/root/system/vendor/firmware/cmnlib.b02 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/cmnlib.b03:recovery/root/system/vendor/firmware/cmnlib.b03 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/cmnlib.mdt:recovery/root/system/vendor/firmware/cmnlib.mdt \
+    $(LOCAL_PATH)/twrp/vendor/firmware/keymaster/keymaster.b00:recovery/root/system/vendor/firmware/keymaster/keymaster.b00 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/keymaster/keymaster.b01:recovery/root/system/vendor/firmware/keymaster/keymaster.b01 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/keymaster/keymaster.b02:recovery/root/system/vendor/firmware/keymaster/keymaster.b02 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/keymaster/keymaster.b03:recovery/root/system/vendor/firmware/keymaster/keymaster.b03 \
+    $(LOCAL_PATH)/twrp/vendor/firmware/keymaster/keymaster.mdt:recovery/root/system/vendor/firmware/keymaster/keymaster.mdt \
+    $(LOCAL_PATH)/twrp/vendor/lib/hw/keystore.msm8226.so:recovery/root/system/vendor/lib/hw/keystore.msm8226.so \
+    $(LOCAL_PATH)/twrp/vendor/lib/libQSEEComAPI.so:recovery/root/system/vendor/lib/libQSEEComAPI.so
+
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
